e1168cdf66f5ce8dcef73bdd7fafda30babb39ed
Yuri Mello <yuridemeello@gmail.com>
2024-06-21 20:16:28 -0300

    fix: adjust date

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,7 +7,7 @@ function Footer() {
     <div className='flex items-center w-full h-28 bg-green p-5'>
       <div className='container flex justify-between w-[1160px]'>
         <Image src={logo} alt='Logo Horta' />
-        <span className='text-white text-base'>2022</span>
+        <span className='text-white text-base'>2024</span>
       </div>
     </div>
   );
